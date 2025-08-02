34b73a01f7b86d977dd228925e3b66f5ddc08ea8
PoyoMain <CLC9512@GMAIL.COM>
2025-08-02 08:18:22 -0400

    Rough Amplifier going

--- Assets/Scripts/Light Pillars/LightAmplifier.cs
+++ Assets/Scripts/Light Pillars/LightAmplifier.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightReflector : MonoBehaviour
+public class LightAmplifier : MonoBehaviour
 {
     [Header("Pillar Settings")]
     [SerializeField] private bool remainCastingAfterActive = false;
     [SerializeField] private bool castOnStart = false;
+    [SerializeField] private bool isOmnidirectional = false;
     [SerializeField] private LayerMask lightLayer;
     [SerializeField] private float lightRadius = 0.25f;
     [SerializeField] private float lightLength = 15f;
@@ -21,18 +22,22 @@ public class LightReflector : MonoBehaviour
     private int lightLevel;
     private List<int> lightsGoingIntoThis;
 
+    private Vector3 directionOfSourceLight;
+
     private void Start()
     {
         lightsGoingIntoThis = new();
-        if (castOnStart) Activate(1);
+        if (castOnStart) Activate(1, transform.forward);
     }
 
-    public void Activate(int levelGoingIn)
+    public void Activate(int levelGoingIn, Vector3 direction)
     {
         if (lightBeam != null) Destroy(lightBeam.gameObject);
         if (currentlyHitObject != null) DeactivateCurrentlyHitReflector();
 
         Active = true;
+
+        directionOfSourceLight = direction;
 
         lightsGoingIntoThis.Add(levelGoingIn);
         lightLevel = CalculateLightLevel();
@@ -50,7 +55,13 @@ public class LightReflector : MonoBehaviour
 
     private void CastLight()
     {
-        bool hitSomething = Physics.SphereCast(lightSpawnPoint.position, lightRadius, lightSpawnPoint.forward, out RaycastHit castHit, lightLength, lightLayer);
+        if (!isOmnidirectional && lightSpawnPoint.forward != directionOfSourceLight)
+        {
+            if (lightBeam != null) Destroy(lightBeam);
+            return;
+        }
+
+        bool hitSomething = Physics.SphereCast(lightSpawnPoint.position, lightRadius, lightSpawnPoint.TransformDirection(lightSpawnPoint.InverseTransformDirection(directionOfSourceLight)), out RaycastHit castHit, lightLength, lightLayer);
 
         // Spawn light
         if (lightBeam == null)
@@ -77,9 +88,8 @@ public class LightReflector : MonoBehaviour
         if (hitSomething)
         {
             lightBeamEndPos = lightSpawnPoint.InverseTransformPoint(castHit.point);
-            lightBeamEndPos.x = 0;
         }
-        else lightBeamEndPos = new Vector3(0, 0, lightLength);
+        else lightBeamEndPos = lightSpawnPoint.TransformDirection(lightSpawnPoint.InverseTransformDirection(directionOfSourceLight)) * lightLength;
 
 
         // Set points on line
@@ -101,18 +111,8 @@ public class LightReflector : MonoBehaviour
 
         if (currentlyHitObject != null) DeactivateCurrentlyHitReflector();
         currentlyHitObject = castHit.transform;
-
-        if (lightLevel <= 0) return;
-
-        if (castHit.transform.TryGetComponent<LightReflector>(out LightReflector hitReflector))
-        {
-            hitReflector.Activate(lightLevel);
-            return;
-        }
-        else if (castHit.transform.TryGetComponent<LightAmplifier>(out LightAmplifier amplifier))
-        {
-            amplifier.Activate(lightLevel, lightSpawnPoint.forward);
-        }        
+        if (!castHit.transform.TryGetComponent<LightReflector>(out LightReflector hitReflector) || lightLevel <= 0) return;
+        hitReflector.Activate(lightLevel);
     }
 
     private void Update()
@@ -129,25 +129,34 @@ public class LightReflector : MonoBehaviour
 
     private void DeactivateCurrentlyHitReflector()
     {
-        if (currentlyHitObject.transform.TryGetComponent<LightReflector>(out LightReflector currentlyHitReflector)) currentlyHitReflector.Deactivate(lightLevel);
-        else if (currentlyHitObject.transform.TryGetComponent<LightAmplifier>(out LightAmplifier currentlyHitAmplifier)) currentlyHitAmplifier.Deactivate(lightLevel);
+        if (currentlyHitObject.transform.TryGetComponent<LightReflector>(out LightReflector currentlyHitReflector))
+        {
+            currentlyHitReflector.Deactivate(lightLevel);
+        }
         currentlyHitObject = null;
     }
 
     private int CalculateLightLevel()
     {
+        if (lightsGoingIntoThis.Count == 0) return 0;
+
         lightsGoingIntoThis.Sort((x, y) => y.CompareTo(x));
 
-        //lightsGoingIntoThis.Sort();
-        //lightsGoingIntoThis.Reverse();
+        int newLightLevel = 0;
 
-        if (lightsGoingIntoThis.Count == 1) return lightsGoingIntoThis[0];
+        if (lightsGoingIntoThis.Count == 1) newLightLevel = lightsGoingIntoThis[0];
         else if (lightsGoingIntoThis.Count >= 2)
         {
-            if (lightsGoingIntoThis[0] == lightsGoingIntoThis[1]) return lightsGoingIntoThis[0] + 1;
+            if (lightsGoingIntoThis[0] == lightsGoingIntoThis[1]) newLightLevel = lightsGoingIntoThis[0] + 1;
             else return lightsGoingIntoThis[0];
         }
 
-        return 0;
+        return newLightLevel + 1;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Physics.SphereCast(lightSpawnPoint.position, lightRadius, lightSpawnPoint.InverseTransformDirection(directionOfSourceLight), out RaycastHit castHit, lightLength, lightLayer);
+        Debug.DrawLine(lightSpawnPoint.position, castHit.point);
     }
 }
