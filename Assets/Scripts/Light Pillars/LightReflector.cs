07298f051c39caecabcdcd2f9b1d429c00e78027
PoyoMain <CLC9512@GMAIL.COM>
2025-08-01 20:33:46 -0400

    Color combinations

--- Assets/Scripts/Light Pillars/LightReflector.cs
+++ Assets/Scripts/Light Pillars/LightReflector.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LightReflector : MonoBehaviour
@@ -17,36 +18,59 @@ public class LightReflector : MonoBehaviour
 
     private LineRenderer lightBeam;
     private Transform currentlyHitObject;
+    private int lightLevel;
+    [SerializeField] private List<int> lightsGoingIntoThis;
 
     private void Start()
     {
-        if (castOnStart) Activate();
+        lightsGoingIntoThis = new();
+        if (castOnStart) Activate(1);
     }
 
     [ContextMenu("Activate")]
-    public void Activate()
+    public void Activate(int levelGoingIn)
     {
+        if (lightBeam != null) Destroy(lightBeam.gameObject);
+        if (currentlyHitObject != null) DeactivateCurrentlyHitReflector();
+
         Active = true;
+
+        lightsGoingIntoThis.Add(levelGoingIn);
+        lightLevel = CalculateLightLevel();
     }
 
     [ContextMenu("Deactivate")]
-    public void Deactivate()
+    public void Deactivate(int levelGoingOut)
     {
-        Active = false;
-
         if (currentlyHitObject != null) DeactivateCurrentlyHitReflector();
+
+        lightsGoingIntoThis.Remove(levelGoingOut);
+        lightLevel = CalculateLightLevel();
+
+        if (lightLevel <= 0) Active = false;
     }
 
     private void CastLight()
     {
         bool hitSomething = Physics.SphereCast(lightSpawnPoint.position, lightRadius, lightSpawnPoint.forward, out RaycastHit castHit, lightLength, lightLayer);
 
-        // Spawn Light
+        // Spawn light
         if (lightBeam == null)
         {
             lightBeam = Instantiate(linePrefab, lightSpawnPoint);
             lightBeam.startWidth = lightBeam.endWidth = lightRadius;
         }
+
+        // Color light
+        Color lightColor = lightLevel switch
+        {
+            0 => Color.white,
+            1 => Color.white,
+            2 => Color.yellow,
+            3 => Color.green,
+            _ => throw new System.NotImplementedException(),
+        };
+        lightBeam.startColor = lightBeam.endColor = lightColor;
 
         // Find end point
         Vector3 lightBeamEndPos;
@@ -77,8 +101,8 @@ public class LightReflector : MonoBehaviour
 
         if (currentlyHitObject != null) DeactivateCurrentlyHitReflector();
         currentlyHitObject = castHit.transform;
-        if (!castHit.transform.TryGetComponent<LightReflector>(out LightReflector hitReflector)) return;
-        hitReflector.Activate();
+        if (!castHit.transform.TryGetComponent<LightReflector>(out LightReflector hitReflector) || lightLevel <= 0) return;
+        hitReflector.Activate(lightLevel);
     }
 
     private void Update()
@@ -90,12 +114,29 @@ public class LightReflector : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (!Active && !remainCastingAfterActive) Destroy(lightBeam);
+        if (!Active && !remainCastingAfterActive) if (lightBeam != null) Destroy(lightBeam.gameObject);
     }
 
     private void DeactivateCurrentlyHitReflector()
     {
-        if (currentlyHitObject.transform.TryGetComponent<LightReflector>(out LightReflector currentlyHitReflector)) currentlyHitReflector.Deactivate();
+        if (currentlyHitObject.transform.TryGetComponent<LightReflector>(out LightReflector currentlyHitReflector)) currentlyHitReflector.Deactivate(lightLevel);
         currentlyHitObject = null;
+    }
+
+    private int CalculateLightLevel()
+    {
+        lightsGoingIntoThis.Sort((x, y) => y.CompareTo(x));
+
+        //lightsGoingIntoThis.Sort();
+        //lightsGoingIntoThis.Reverse();
+
+        if (lightsGoingIntoThis.Count == 1) return lightsGoingIntoThis[0];
+        else if (lightsGoingIntoThis.Count >= 2)
+        {
+            if (lightsGoingIntoThis[0] == lightsGoingIntoThis[1]) return lightsGoingIntoThis[0] + 1;
+            else return lightsGoingIntoThis[0];
+        }
+
+        return 0;
     }
 }
