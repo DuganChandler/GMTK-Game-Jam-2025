21afa554f9647d537900f5922f05d022c2482c39
DuganChandler <duganchandler10@gmail.com>
2025-08-02 16:47:53 -0400

    fixed pushable stuff

--- Assets/Scripts/Objects/Interactable Objects/PushablePullableObj.cs
+++ Assets/Scripts/Objects/Interactable Objects/PushablePullableObj.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class PushableObject : MonoBehaviour, IInteractable<PlayerController> {
     [Tooltip("Units per second along the chosen axis")]
     public float pushSpeed = 3f;
+    private Rigidbody rb;
 
     void Awake() {
-        var rb = GetComponent<Rigidbody>();
-        rb.isKinematic = true;
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;                                // ← dynamic now
         rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.collisionDetectionMode = CollisionDetectionMode.Discrete; 
     }
 
     public IEnumerator Interact(Transform playerT, PlayerController playerController) {
         if (!playerController) yield break;
 
-        playerT.SetParent(transform, worldPositionStays: true);
+        if (!playerController.TryGetComponent<Rigidbody>(out var playerRb)) yield break;
+
+        rb.isKinematic = false;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
 
         Vector3 toPlayer = playerT.position - transform.position;
         float dotR = Vector3.Dot(toPlayer, transform.right);
@@ -31,12 +36,16 @@ public class PushableObject : MonoBehaviour, IInteractable<PlayerController> {
             float f = playerController.MoveInput.z;
             if (Mathf.Abs(f) > 0.1f) {
                 Vector3 dir = -sideSign * f * slideAxis;
-                transform.position += pushSpeed * Time.fixedDeltaTime * dir;
+                Vector3 delta = pushSpeed * Time.fixedDeltaTime * dir;
+
+                rb.MovePosition(transform.position + delta);
+                playerRb.MovePosition(playerT.position + delta);
             }
 
             yield return new WaitForFixedUpdate();
         }
 
-        playerT.SetParent(null, worldPositionStays: true);
+        rb.isKinematic = true;
+        rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
     }
 }
